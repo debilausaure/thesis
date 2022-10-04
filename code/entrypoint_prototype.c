c80ad52a91628d232f5b759a704c0f44c4b1b844
Florian Vanhems <florian.vanhems@univ-lille.fr>
2022-10-04 23:01:15 +0200

    avancement

---
+++ code/entrypoint_prototype.c
@@ -0,0 +1,5 @@
+yield_checks cg_yieldGlue(vaddr calleePartDescVAddr,
+                          userValue userTargetInterrupt,
+                          userValue userCallerContextSaveIndex,
+                          interruptMask flagsOnYield,
+                          interruptMask flagsOnWake);
